52b2f3b36b3233cdef1122364642f450cd997bac
Ilgiz <karabaev.il@yandex.ru>
2020-04-24 19:34:35 +0300

    FIX

--- resources/assets/js/components/ChatComponent.vue
+++ resources/assets/js/components/ChatComponent.vue
@@ -45,13 +45,10 @@
                     alert('Ваш браузер не поддерживает HTML Notifications, его необходимо обновить.');
                 } else if (Notification.permission === "granted") {
                     var notification = new Notification(title, options);
-                    function clickFunc() { alert('Пользователь кликнул на уведомление');}
-                    notification.onclick = clickFunc;
                 } else if (Notification.permission !== 'denied') {
                     Notification.requestPermission(function (permission) {
                         if (permission === "granted") {
                             var notification = new Notification(title, options);
-
                         } else {
                             alert('Вы запретили показывать уведомления');
                         }
