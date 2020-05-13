eb9218e49b2c9ca571b3019be06ce94d954021a0
Ilgiz <karabaev.il@yandex.ru>
2020-05-13 08:50:25 +0300

    FIX

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,17 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function () {
-            $token =env('VK_TOKEN');
 
-            $message = 'Вам сообщение от sdfdfs: 
-        
-        dgfgdfgdfgdfgdf
-        
-         https://ilgizkar.ru/home';
-
-            $query = file_get_contents("https://api.vk.com/method/messages.send?user_id=454162779&message=".urlencode($message)."&v=5.37&access_token=".$token);
-        });
     }
 
     /**
--- public/cron.php
+++ public/cron.php
@@ -2,11 +2,7 @@
 function cron() {
     $token = '0b9295c4d383cb0c4bb30332a01aa5cf4762afb777ab07c7371d43c98d95a49712750530d22921557b4e1';
 
-    $message = 'Вам сообщение от sdfdfs: 
-        
-        dgfgdfgdfgdfgdf
-        
-         https://ilgizkar.ru/home';
+    $message = 'Ежедневное оповещение';
 
     $query = file_get_contents("https://api.vk.com/method/messages.send?user_id=454162779&message=".urlencode($message)."&v=5.37&access_token=".$token);
 }
